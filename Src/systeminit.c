866513b13806a236ad1197b54673e74946d07f88
Hari Udayakumar <hariudkmr@gmail.com>
2021-04-23 14:51:24 +0800

    Usb Fw developement: Initial Project Settings 1. Base Project tested with STM32F407VG Discovery Board 2. SWO enabled and set to 16MHz 3. STLink Debugger used

---
+++ Src/systeminit.c
@@ -0,0 +1,16 @@
+/*
+ * systeminit.c
+ *
+ *  Created on: 22 Apr 2021
+ *      Author: hari
+ */
+
+#include <logger.h>
+
+LogLevel system_log_level = LOG_LEVEL_DEBUG;
+
+void SystemInit(void)
+{
+//	configure_mco1();
+//	configure_clock();
+}
