69e28b27be11990f619a8dfbceb15e55c5afbc0b
Hari Udayakumar <hari.udayakumar@continetal-corporation.com>
2021-04-27 19:21:16 +0800

    USB FW: Added Device Descriptor data structure 1. Defined different descriptor type, class type , subclass type and protocol type

--- Inc/usb_standards.h
+++ Inc/usb_standards.h
@@ -74,4 +74,72 @@ typedef struct
 #define USB_BM_REQUEST_TYPE_RECIPIENT_OTHER (3 << 0)
 /**@}*/
 
+/** \name USB standard descriptor types
+ * @{ */
+#define USB_DESCRIPTOR_TYPE_DEVICE 0x01
+#define USB_DESCRIPTOR_TYPE_CONFIGURATION 0x02
+#define USB_DESCRIPTOR_TYPE_STRING 0x03
+#define USB_DESCRIPTOR_TYPE_INTERFACE 0x04
+#define USB_DESCRIPTOR_TYPE_ENDPOINT 0x05
+#define USB_DESCRIPTOR_TYPE_QUALIFIER 0x06
+#define USB_DESCRIPTOR_TYPE_OTHER 0x07
+#define USB_DESCRIPTOR_TYPE_INTERFACEPOWER 0x08
+#define USB_DESCRIPTOR_TYPE_OTG 0x09
+#define USB_DESCRIPTOR_TYPE_DEBUG 0x0A
+#define USB_DESCRIPTOR_TYPE_INTERFASEASSOC 0x0B
+#define USB_DESCRIPTOR_TYPE_CS_INTERFACE 0x24
+#define USB_DESCRIPTOR_TYPE_CS_ENDPOINT 0x25
+/** @} */
+
+/**\name USB classes
+ * @{ */
+#define USB_CLASS_PER_INTERFACE 0x00 /**<\brief Class defined on interface level.*/
+#define USB_CLASS_AUDIO 0x01 /**<\brief Audio device class.*/
+#define USB_CLASS_PHYSICAL 0x05 /**<\brief Physical device class.*/
+#define USB_CLASS_STILL_IMAGE 0x06 /**<\brief Still Imaging device class.*/
+#define USB_CLASS_PRINTER 0x07 /**<\brief Printer device class.*/
+#define USB_CLASS_MASS_STORAGE 0x08 /**<\brief Mass Storage device class.*/
+#define USB_CLASS_HUB 0x09 /**<\brief HUB device class.*/
+#define USB_CLASS_CSCID 0x0B /**<\brief Smart Card device class.*/
+#define USB_CLASS_CONTENT_SEC 0x0D /**<\brief Content Security device class.*/
+#define USB_CLASS_VIDEO 0x0E /**<\brief Video device class.*/
+#define USB_CLASS_HEALTHCARE 0x0F /**<\brief Personal Healthcare device class.*/
+#define USB_CLASS_AV 0x10 /**<\brief Audio/Video device class.*/
+#define USB_CLASS_BILLBOARD 0x11 /**<\brief Billboard device class.*/
+#define USB_CLASS_CBRIDGE 0x12 /**<\brief USB Type-C Bridge device class.*/
+#define USB_CLASS_DIAGNOSTIC 0xDC /**<\brief Diagnostic device class.*/
+#define USB_CLASS_WIRELESS 0xE0 /**<\brief Wireless controller class.*/
+#define USB_CLASS_MISC 0xEF /**<\brief Miscellanious device class.*/
+#define USB_CLASS_IAD 0xEF /**<\brief Class defined on interface association level.*/
+#define USB_CLASS_APP_SPEC 0xFE /**<\brief Application Specific class.*/
+#define USB_CLASS_VENDOR 0xFF /**<\brief Vendor specific class.*/
+#define USB_CLASS_HID 0x03
+
+#define USB_SUBCLASS_NONE 0x00 /**<\brief No subclass defined.*/
+#define USB_SUBCLASS_IAD 0x02 /**<\brief Subclass defined on interface association level.*/
+#define USB_SUBCLASS_VENDOR 0xFF /**<\brief Vendor specific subclass.*/
+
+#define USB_PROTOCOL_NONE 0x00 /**<\brief No protocol defined.*/
+#define USB_PROTOCOL_IAD 0x01 /**<\brief Protocol defined on interface association level.*/
+#define USB_PROTOCOL_VENDOR 0xFF /**<\brief Vendor specific protocol.*/
+/** @} */
+
+/**\brief USB device descriptor. */
+typedef struct {
+	uint8_t  bLength; /**<\brief Size of the descriptor (in bytes).*/
+	uint8_t  bDescriptorType; /**<\brief \ref USB_DESCRIPTOR_TYPE_DEVICE device descriptor.*/
+	uint16_t bcdUSB; /**<\brief USB specification release number.*/
+	uint8_t  bDeviceClass; /**<\brief USB device class.*/
+	uint8_t  bDeviceSubClass; /**<\brief USB device subclass.*/
+	uint8_t  bDeviceProtocol; /**<\brief USB device protocol.*/
+	uint8_t  bMaxPacketSize0; /**<\brief Max packet size for the control endpoint 0 (8, 16, 32 or 64 bytes).*/
+	uint16_t idVendor; /**<\brief Vendor ID for the USB device.*/
+	uint16_t idProduct; /**<\brief Product ID for the USB device.*/
+	uint16_t bcdDevice; /**<\brief Device release number.*/
+	uint8_t  iManufacturer; /**<\brief String descriptor index for the manufacturer's name.*/
+	uint8_t  iProduct; /**<\brief String descriptor index for the product name.*/
+	uint8_t  iSerialNumber; /**<\brief String descriptor index for the product serial number.*/
+	uint8_t  bNumConfigurations; /**<\brief Total number of configurations supported by the USB device.*/
+} __attribute__((__packed__)) UsbDeviceDescriptor;
+
 #endif /* USB_STANDARDS_H_ */
